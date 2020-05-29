ed8b27de5ae60ea9fe52dffe2025a51a93b1e7b6
Lucas Briatte Vatel <lucasbriatte@gmail.com>
2020-05-29 09:53:19 +0200

    Adding basic GUI

--- src/fr/polytech/Main.java
+++ src/fr/polytech/Main.java
@@ -1,8 +1,38 @@
 package fr.polytech;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-	// write your code here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            // Localisation du fichier FXML.
+            final URL url = getClass().getResource("gui.fxml");
+            // Création du loader.
+            final FXMLLoader fxmlLoader = new FXMLLoader(url);
+            // Chargement du FXML.
+            final HBox root = (HBox) fxmlLoader.load();
+            // Création de la scène.
+            final Scene scene = new Scene(root, 800, 400);
+            primaryStage.setScene(scene);
+        } catch (IOException ex) {
+            System.err.println("Erreur au chargement: " + ex);
+        }
+        primaryStage.setTitle("Graphical Editor");
+        primaryStage.show();
     }
 }

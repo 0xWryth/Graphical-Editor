71f8c3e8fcfbd13699cd2f07da08c1b49de4a1bf
Lucas Briatte Vatel <lucasbriatte@gmail.com>
2020-05-29 20:44:06 +0200

    Removing unused imports

--- src/fr/polytech/Main.java
+++ src/fr/polytech/Main.java
@@ -3,11 +3,7 @@ package fr.polytech;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
